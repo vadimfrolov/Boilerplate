149b2b12417b54bc5aaa0e4c733b5878b5d2fab2
Vadim Frolov <vadimf74@ya.ru>
2021-11-08 22:12:35 +0300

    nginx fix
    Now you can start nginx static server

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = () => {
   return (
     <>
-      <div>welcom to boilerplate</div>
+      <div className="title">welcome to boilerplate</div>
     </>
   );
 };
